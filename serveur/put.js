4f1b8f518b6a4fbfe47af9f734f4682840a365bf
Ashley <e2089834@cmaisonneuve.qc.ca>
2026-03-31 20:02:01 -0400

    mise a jour de put.js

--- serveur/put.js
+++ serveur/put.js
@@ -4,7 +4,6 @@ const knex = require('knex');
 const app = express();
 app.use(express.json());
 
-// ---- DB CONFIG ----
 const db = knex({
   client: 'sqlite3',
   connection: {
@@ -13,10 +12,8 @@ const db = knex({
   useNullAsDefault: true
 });
 
-// Enable foreign keys
 db.raw('PRAGMA foreign_keys = ON');
 
-// ---- UPDATE HELPERS ----
 const updateById = (table, idField) => async (req, res) => {
   try {
     const { id } = req.params;
@@ -39,14 +36,12 @@ const updateById = (table, idField) => async (req, res) => {
   }
 };
 
-// ---- PUT ROUTES ----
 app.put('/roles/:id', updateById('role', 'id_role'));
 app.put('/employes/:id', updateById('employe', 'id_employe'));
 app.put('/clients/:id', updateById('client', 'id_client'));
 app.put('/voitures/:id', updateById('voiture', 'id_voiture'));
 app.put('/payements/:id', updateById('payement', 'id_payement'));
 
-// ---- START SERVER ----
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
