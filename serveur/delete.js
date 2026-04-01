caffd8eadcad5d5d5c1eae80c2773b4b0bed3d40
N-VD <e6316729@cmaisonneuve.qc.ca>
2026-03-31 21:11:56 -0400

    ajout info si donner vide

--- serveur/delete.js
+++ serveur/delete.js
@@ -10,6 +10,12 @@ app.delete('/payment/:id', async (req, res) => {
 
     try {
         const result = await db('payement').where({ id_payement: id }).del();
+        if (result === 0) {
+            // Si 0 ligne supprimée, l'ID n'existe pas en base
+            return res.status(404).json({ 
+                message: "Erreur : Ce paiement n'existe pas ou a déjà été supprimé." 
+            });
+        }
         res.json({ message: 'Payment supprimé avec succès', result });
     } catch (err) {
         res.status(500).json(err);
@@ -21,6 +27,12 @@ app.delete('/client/:id', async (req, res) => {
 
     try {
         const result = await db('client').where({ id_client: id }).del();
+        if (result === 0) {
+            // Si 0 ligne supprimée, l'ID n'existe pas en base
+            return res.status(404).json({ 
+                message: "Erreur : Ce client n'existe pas ou a déjà été supprimé." 
+            });
+        }
         res.json({ message: 'Client supprimé avec succès', result });
     } catch (err) {
         res.status(500).json(err);
@@ -32,6 +44,12 @@ app.delete('/voiture/:id', async (req, res) => {
 
     try {
         const result = await db('voiture').where({ id_voiture: id }).del();
+        if (result === 0) {
+            // Si 0 ligne supprimée, l'ID n'existe pas en base
+            return res.status(404).json({ 
+                message: "Erreur : Cette voiture n'existe pas ou a déjà été supprimé." 
+            });
+        }
         res.json({ message: 'Voiture supprimée avec succès', result });
     } catch (err) {
         res.status(500).json(err);
@@ -43,6 +61,12 @@ app.delete('/role/:id', async (req, res) => {
 
     try {
         const result = await db('role').where({ id_role: id }).del();
+        if (result === 0) {
+            // Si 0 ligne supprimée, l'ID n'existe pas en base
+            return res.status(404).json({ 
+                message: "Erreur : Ce role n'existe pas ou a déjà été supprimé." 
+            });
+        }
         res.json({ message: 'Role supprimé avec succès', result });
     } catch (err) {
         res.status(500).json(err);
@@ -54,6 +78,12 @@ app.delete('/employe/:id', async (req, res) => {
 
     try {
         const result = await db('employe').where({ id_employe: id }).del();
+        if (result === 0) {
+            // Si 0 ligne supprimée, l'ID n'existe pas en base
+            return res.status(404).json({ 
+                message: "Erreur : Cet employe n'existe pas ou a déjà été supprimé." 
+            });
+        }
         res.json({ message: 'Employe supprimé avec succès', result });
     } catch (err) {
         res.status(500).json(err);
